fc1ce27553724efea1f5431395a087deda667683
inunotaisho26 <ebfairweather@gmail.com>
2017-08-21 16:40:16 -0400

    pushing beginning setup of typescripting

---
+++ README.ja.md
@@ -0,0 +1,7 @@
+** **
+[何がTypeScriptです。]
+
+
+1. [Node.js](https://nodejs.org/ja/)がインストール
+2. [TypeScript](http://www.typescriptlang.org/)がインストール
+3. `npm install typescripting -g`
---
+++ typescripting.js
@@ -0,0 +1,9 @@
+const typescripting = require('workshopper-adventure')({
+    appDir: __dirname,
+    languages: ['en', 'es', 'fr', 'ja', 'zh-cn', 'zh-tw', 'pt-br', 'ru', 'uk', 'vi', 'ko', 'nb-no', 'it'],
+    header: require('workshpper-adventure/default/header'),
+    footer: require('workshpper-adventure/default/footer'),
+    fail: require('workshpper-adventure/default/fail'),
+    pass: require('workshpper-adventure/default/pass')
+})
+
